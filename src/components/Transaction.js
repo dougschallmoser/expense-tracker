05fae42e0f3508892a8276394b68a36474cb28ed
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-24 11:58:35 -0800

    reformat code for easier reading

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -9,7 +9,13 @@ function Transaction({ transaction }) {
 
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
-      {transaction.subject} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-transaction" onClick={() => deleteTransaction(transaction.id)}>x</button>
+      {transaction.subject} <span>{sign}${Math.abs(transaction.amount)}</span>
+      <button
+        className="delete-transaction"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
     </li>
   )
 }
