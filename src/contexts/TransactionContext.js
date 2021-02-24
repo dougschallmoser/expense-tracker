f56171f701c185d812bfaef8891baa8b746f50a8
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-24 12:26:32 -0800

    integrate localStorage for holding transaction list

--- src/contexts/TransactionContext.js
+++ src/contexts/TransactionContext.js
@@ -1,9 +1,11 @@
 import React, { createContext, useReducer } from 'react';
-import axios from 'axios';
 import TransactionReducer from './TransactionReducer'
+// import axios from 'axios';
+
+const storageTransactions = localStorage.getItem('transactions') ? JSON.parse(localStorage.getItem('transactions')) : [] 
 
 const initialState = {
-  transactions: [],
+  transactions: storageTransactions,
   loading: true
 }
 
@@ -28,27 +30,27 @@ export const TransactionContextProvider = ({ children }) => {
   }
 
   // Async Action
-  async function getTransactions() {
-    try {
-      const response = await axios.get('')
-      dispatch({
-        type: 'GET_TRANSACTIONS',
-        payload: response.data.data
-      })
-    } catch (err) {
-      dispatch({
-        type: 'TRANSACTION_ERROR',
-        payload: err.response.data.error
-      })
-    }
-  }
+  // async function getTransactions() {
+  //   try {
+  //     const response = await axios.get('')
+  //     dispatch({
+  //       type: 'GET_TRANSACTIONS',
+  //       payload: response.data.data
+  //     })
+  //   } catch (err) {
+  //     dispatch({
+  //       type: 'TRANSACTION_ERROR',
+  //       payload: err.response.data.error
+  //     })
+  //   }
+  // }
 
   return (
     <TransactionContext.Provider value={{
       transactions: state.transactions,
       addTransaction,
       deleteTransaction,
-      getTransactions,
+      // getTransactions,
       error: state.error,
       loading: state.loading
 
--- src/contexts/TransactionReducer.js
+++ src/contexts/TransactionReducer.js
@@ -1,4 +1,8 @@
-function AppReducer(state, action) {
+const setStorage = (transactions) => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.length > 0 ? transactions : []))
+}
+
+function TransactionReducer(state, action) {
   switch (action.type) {
     case 'GET_TRANSACTIONS':
       return {
@@ -6,24 +10,35 @@ function AppReducer(state, action) {
         loading: false,
         transactions: action.payload
       }
+
     case 'DELETE_TRANSACTION':
+      const removedTransaction = state.transactions.find(trans => trans.id !== action.payload);
+      state.transactions.splice(state.transactions.indexOf(removedTransaction), 1);
+      setStorage(state.transactions);
+
       return {
         ...state,
-        transactions: state.transactions.filter(trans => trans.id !== action.payload)
+        transactions: [...state.transactions]
       }
+
     case 'ADD_TRANSACTION':
+      state.transactions.unshift(action.payload);
+      setStorage(state.transactions);
+
       return {
         ...state,
-        transactions: [action.payload,  ...state.transactions]
+        transactions: [...state.transactions]
       }
+
     case 'TRANSACTION_ERROR':
       return {
         ...state,
         error: action.payload
       }
+
     default: 
       return state
   }
 }
 
-export default AppReducer;
+export default TransactionReducer;
