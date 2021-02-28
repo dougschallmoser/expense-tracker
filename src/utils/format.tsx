a9249c322a893041dac0f17778ccac6ce3d4fcee
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-27 18:58:55 -0800

    rename function, change parameter from number to string

--- src/utils/format.tsx
+++ src/utils/format.tsx
@@ -1,3 +1,3 @@
-export function numberWithCommas(x: number) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+export function addCommas(amount: string) {
+  return amount.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
