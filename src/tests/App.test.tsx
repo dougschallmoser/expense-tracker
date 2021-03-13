f9e6bc2a6e393e1d6eb3c8012994b8c390f2ed28
Doug Schallmoser <dougschallmoser@gmail.com>
2021-03-12 16:08:53 -0800

    build App component tests to check title appears on page and formatAmount function formats currency correcty

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+import { formatAmount } from '../utils/format';
+
+test('name of app displays on page', () => {
+  render(<App />)
+  
+  const title = screen.getByRole('heading', { name: `Doug's Expense Tracker` })
+  expect(title).toBeInTheDocument()
+})
+
+test('formatAmount function formats currency with commas', () => {
+  const inputAmt = '4321.67'
+  const formattedAmt = formatAmount(inputAmt)
+  expect(formattedAmt).toBe('4,321.67')
+
+  const inputAmtTwo = '121870.02'
+  const formattedAmtTwo = formatAmount(inputAmtTwo)
+  expect(formattedAmtTwo).toBe('121,870.02')
+})
+
