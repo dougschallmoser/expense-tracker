33f44b7987c537f8709d80a3689c9fd65a7dc205
Doug Schallmoser <dougschallmoser@gmail.com>
2021-03-15 13:42:42 -0700

    refactor function typing in ApplicationState interface

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -7,8 +7,8 @@ export interface ITransaction {
 export interface ApplicationState {
   transactions: ITransaction[],
   error?: string,
-  addTransaction?: (transaction: ITransaction) => void,
-  deleteTransaction?: (id: number) => void,
+  addTransaction?(transaction: ITransaction): void,
+  deleteTransaction?(id: number): void,
   loading: boolean
 }
 
